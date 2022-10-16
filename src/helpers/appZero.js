82a80b0ec0fede0aa1870f7ebe2faa4035c73cb5
Kelvin F. Baez Rogriguez <kelvin12378945610@gmail.com>
2022-10-16 03:12:18 -0400

    [refactoring] addZero helper

--- src/helpers/appZero.js
+++ src/helpers/appZero.js
@@ -1,12 +1,8 @@
 export const addZero = (setFormData, inputName, inputValue, amountZero) => {
+  if (inputValue.replace(/\s/g, "").length > 16) return;
+
   const result =
-    inputValue +
-    "0".repeat(
-      amountZero -
-        (inputValue.replace(/\s/g, "").length > 16
-          ? 16
-          : inputValue.replace(/\s/g, "").length)
-    );
+    inputValue + "0".repeat(amountZero - inputValue.replace(/\s/g, "").length);
 
   setFormData((formData) => ({
     ...formData,
